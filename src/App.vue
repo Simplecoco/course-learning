bdf83589e7756adabe780a010a635f8d9c3cf8dd
Simplecoco <dkdsh@163.com>
2019-01-05 14:36:27 +0800

    feature: 引入element, 路由细节修改等

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div> -->
+    <!-- <router-link to="/">home</router-link>
+    <router-link to="/about">about</router-link> -->
     <router-view/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
-<style lang="less">
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
-#nav {
+/* #nav {
   padding: 30px;
   a {
     font-weight: bold;
@@ -25,5 +42,5 @@
       color: #42b983;
     }
   }
-}
+} */
 </style>
