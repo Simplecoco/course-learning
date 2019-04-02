b92672722ccf1f0dd5ac25befcde721ba7b35e12
Simplecoco <dkdsh@163.com>
2019-04-02 14:47:32 +0800

    feature: 用户加入抽屉聊天ui

--- src/views/User/index.vue
+++ src/views/User/index.vue
@@ -3,17 +3,33 @@
     <Layout :menuItems="menuItems">
       <router-view/>
     </Layout>
+    <div class="chat-drawer">
+      <Button class="drawer-btn"
+        @click="drawer.show = true" 
+        type="success" 
+        shape="circle"
+        size="large"
+        icon="md-chatboxes"
+        style="width: 50px; height: 50px"
+      >
+      </Button>
+      <Drawer title="在线交流" width="35" :closable="false" v-model="drawer.show">
+        <chat-card></chat-card>
+      </Drawer>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Layout from '@/components/Layout.vue'
+import ChatCard from '@/components/ChatCard.vue'
 
 export default {
   name: 'user',
   components: {
-    Layout
+    Layout,
+    ChatCard
   },
   data () {
     return {
@@ -38,8 +54,25 @@ export default {
           icon: 'md-chatboxes',
           name: 'chat'
         }
-      ]
+      ],
+      drawer: {
+        show: false
+      }
     }
   }
 }
 </script>
+<style lang="less" scoped>
+  .drawer-btn {
+    position: fixed;
+    bottom: 40px;
+    right: 40px;
+    z-index: 99;
+    box-shadow: 3px 3px 10px #dcdee2;
+    transform: scale(1);
+    transition: transform .3s;
+    &:hover {
+      transform: scale(1.2);
+    }
+  }
+</style>
